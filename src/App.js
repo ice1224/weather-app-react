5047afef4b369da59de06da8c667aae38607213e
ice1224 <mariuszkobiela1224@gmail.com>
2019-11-09 02:13:07 +0100

    Adding state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Weather from './components/Weather';
 const API_KEY = "b4195a6ab2f69228045387b160d8ed7e";
 
 class App extends React.Component {
+  
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
 
   getWeather = async (e) => {
     e.preventDefault();
@@ -14,6 +23,15 @@ class App extends React.Component {
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     console.log(data);
+    
+    this.setState({
+      city: data.name,
+      country: data.sys.country,
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    })
   }
 
   render(){
