6e4fb0faab2983671a0ea7d56b4eb43d1273eaff
ice1224 <mariuszkobiela1224@gmail.com>
2019-11-09 02:06:12 +0100

    Handling weather api and getting input

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "b4195a6ab2f69228045387b160d8ed7e";
+
 class App extends React.Component {
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
   render(){
     return (
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather={this.getWeather}/>
         <Weather/>
       </div>
     );
