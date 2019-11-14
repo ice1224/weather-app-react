c4eecc4113b844ba1084f4ed5d73a8218302b15d
ice1224 <mariuszkobiela1224@gmail.com>
2019-11-14 01:56:24 +0100

    Changing text

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from "react";
 const Titles = () => (
     <div>
         <h1 className="title-container__title">Weather App</h1>
-        <p className="title-container__subtitle">Find out current weather simply and accurately</p>
+        <p className="title-container__subtitle">Get current weather simply and accurately</p>
     </div>
 )
 
